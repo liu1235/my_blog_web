dc8e04d81d2adb7fe673ee24e08626be317a81c4
liuzw <670115782@qq.com>
2019-05-06 19:16:02 +0800

    初始化

---
+++ src/router/route.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './index'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+});
+
+let routerApi = ['/userInfo'];
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    sessionStorage.removeItem('userInfo');
+  }
+  let user = JSON.parse(sessionStorage.getItem('userInfo'));
+  if (!user && to.path !== '/login') {
+    if(routerApi.indexOf(to.path) > -1) {
+      next({ path: '/login' });
+    } else {
+      next();
+    }
+  } else {
+    alert(to.path)
+      if (to.path) {
+        next();
+      } else {
+        next({ path: '/404' });
+      }
+  }
+});
+
+export default router;
